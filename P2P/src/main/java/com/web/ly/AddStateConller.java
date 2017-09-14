8cb6498c4d6864b0ccfabacaccfdf588a8c07cda
LiJinLiang <493372158@qq.com>
2017-09-14 10:12:03 +0800

    hebing

--- P2P/src/main/java/com/web/ly/AddStateConller.java
+++ P2P/src/main/java/com/web/ly/AddStateConller.java
@@ -61,17 +61,17 @@ public class AddStateConller{
 		
 		int emp=addEmployeeService.upProEmplo(projectsid);//给项目添加一个审批人
 		
-		/*int flag=SinAuditing.selProTy(projectsid);//根据项目序号，查询该项目属于个人还是机构
+		int flag=SinAuditing.selProTy(projectsid);//根据项目序号，查询该项目属于个人还是机构
 			//System.out.println("-----------aaa:"+flag);
 		if (flag==1) {
 			int sin=SinAuditing.inserSinSt(projectsid);//在所有项目里点击审核，给个人信息审核表一个默认 未审核 的初始值	（1）		
 		}else{
 			int org=SinAuditing.inserOrSt(projectsid);//在所有项目里点击审核，给机构信息审核表一个默认 未审核 的初始值（1）
 		}
-		int pro2=SinAuditing.insertProInSt(projectsid);//在所有项目里点击审核，给机构信息审核表一个默认 未审核 的初始值（2）
-		int pro3=SinAuditing.inserProDeSt(projectsid);//在所有项目里点击审核，给 项目描述 审核表一个默认 未审核 的初始值（3）
-		int pro4=SinAuditing.inserProReSt(projectsid);//在所有项目里点击审核，给 项目回报  审核表一个默认 未审核 的初始值（4）
-*/	}
+			int pro2=SinAuditing.insertProInSt(projectsid);//在所有项目里点击审核，给机构信息审核表一个默认 未审核 的初始值（2）
+			int pro3=SinAuditing.inserProDeSt(projectsid);//在所有项目里点击审核，给 项目描述 审核表一个默认 未审核 的初始值（3）
+			int pro4=SinAuditing.inserProReSt(projectsid);//在所有项目里点击审核，给 项目回报  审核表一个默认 未审核 的初始值（4）
+		}
 	
 	
 	/**
